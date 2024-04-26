d42c70196b1fc597542b65f33bf4f7b436f73676
cassiusw <cassiuswillis@gmail.com>
2024-04-26 14:29:24 -0400

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,14 +4,16 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{}<input type="number" step="10" value={newBudget} onChange={handleBudgetChange} /></span>
+        </div>
     );
 };
+
 export default Budget;
